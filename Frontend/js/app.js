53a67d2eed5903b3d9fa6158cd2c8ba17219bbc5
VictoriaStolp <vfstolpman42@tntech.edu>
2026-05-01 13:29:21 -0500

    Edited the UI to have an add info page

---
+++ Frontend/js/app.js
@@ -0,0 +1,6 @@
+document.querySelector('#btnAdd').addEventListener('click', ()=>{
+    //This changes the title back
+    document.title = "AddResumeInfo-Options"
+    document.querySelector('#divHomePage').style = "display:none"
+    document.querySelector('#divAddResumeInfo').style = "display:block"
+})
