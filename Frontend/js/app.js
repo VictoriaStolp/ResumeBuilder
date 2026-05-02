beab0a552be0129bf1147b568763de680ed9e5b4
VictoriaStolp <vfstolpman42@tntech.edu>
2026-05-02 15:03:19 -0500

    Created the frontend forms for all databases except the responsibility ones

--- Frontend/js/app.js
+++ Frontend/js/app.js
@@ -4,3 +4,94 @@ document.querySelector('#btnAdd').addEventListener('click', ()=>{
     document.querySelector('#divHomePage').style = "display:none"
     document.querySelector('#divAddResumeInfo').style = "display:block"
 })
+
+document.querySelector('#btnJobs').addEventListener('click', ()=>{
+    //This changes the title back
+    document.title = "Add Job Information"
+    document.querySelector('#divAddResumeInfo').style = "display:none"
+    document.querySelector('#divAddJobInfo').style = "display:block"
+})
+
+document.querySelector('#btnBack').addEventListener('click', ()=>{
+    //This changes the title back
+    document.title = "WorkingGirlie-ResumeBuilder"
+    document.querySelector('#divAddResumeInfo').style = "display:none"
+    document.querySelector('#divHomePage').style = "display:block"
+})
+
+document.querySelector('#btnBackToResumeInfo1').addEventListener('click', ()=>{
+    //This changes the title back
+    document.title = "AddResumeInfo-Options"
+    document.querySelector('#divAddJobInfo').style = "display:none"
+    document.querySelector('#divAddResumeInfo').style = "display:block"
+})
+
+document.querySelector('#btnEducation').addEventListener('click', ()=>{
+    //This changes the title back
+    document.title = "Add Education Information"
+    document.querySelector('#divAddResumeInfo').style = "display:none"
+    document.querySelector('#divAddEduInfo').style = "display:block"
+})
+
+document.querySelector('#btnBackToResumeInfo2').addEventListener('click', ()=>{
+    //This changes the title back
+    document.title = "AddResumeInfo-Options"
+    document.querySelector('#divAddEduInfo').style = "display:none"
+    document.querySelector('#divAddResumeInfo').style = "display:block"
+})
+
+document.querySelector('#btnInvolvement').addEventListener('click', ()=>{
+    //This changes the title back
+    document.title = "Add Involvement Information"
+    document.querySelector('#divAddResumeInfo').style = "display:none"
+    document.querySelector('#divAddInvInfo').style = "display:block"
+})
+
+document.querySelector('#btnBackToResumeInfo3').addEventListener('click', ()=>{
+    //This changes the title back
+    document.title = "AddResumeInfo-Options"
+    document.querySelector('#divAddInvInfo').style = "display:none"
+    document.querySelector('#divAddResumeInfo').style = "display:block"
+})
+
+document.querySelector('#btnSkill').addEventListener('click', ()=>{
+    //This changes the title back
+    document.title = "Add Skill Information"
+    document.querySelector('#divAddResumeInfo').style = "display:none"
+    document.querySelector('#divAddSkillInfo').style = "display:block"
+})
+
+document.querySelector('#btnBackToResumeInfo4').addEventListener('click', ()=>{
+    //This changes the title back
+    document.title = "AddResumeInfo-Options"
+    document.querySelector('#divAddSkillInfo').style = "display:none"
+    document.querySelector('#divAddResumeInfo').style = "display:block"
+})
+
+document.querySelector('#btnCertification').addEventListener('click', ()=>{
+    //This changes the title back
+    document.title = "Add Certification Information"
+    document.querySelector('#divAddResumeInfo').style = "display:none"
+    document.querySelector('#divAddCertInfo').style = "display:block"
+})
+
+document.querySelector('#btnBackToResumeInfo5').addEventListener('click', ()=>{
+    //This changes the title back
+    document.title = "AddResumeInfo-Options"
+    document.querySelector('#divAddCertInfo').style = "display:none"
+    document.querySelector('#divAddResumeInfo').style = "display:block"
+})
+
+document.querySelector('#btnAward').addEventListener('click', ()=>{
+    //This changes the title back
+    document.title = "Add Award Information"
+    document.querySelector('#divAddResumeInfo').style = "display:none"
+    document.querySelector('#divAddAwardInfo').style = "display:block"
+})
+
+document.querySelector('#btnBackToResumeInfo6').addEventListener('click', ()=>{
+    //This changes the title back
+    document.title = "AddResumeInfo-Options"
+    document.querySelector('#divAddAwardInfo').style = "display:none"
+    document.querySelector('#divAddResumeInfo').style = "display:block"
+})
